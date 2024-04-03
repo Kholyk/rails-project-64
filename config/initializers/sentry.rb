cda6e4cd297f9bd658c3d0105d54a04c9970e71f
Oleg Kholyk <i@kholyk.ru>
2024-04-03 09:34:32 +0300

    fix: rubocop offences

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,13 +1,15 @@
+# frozen_string_literal: true
+
 Sentry.init do |config|
   config.dsn = 'https://815a0879d2b1230842de294e6a2e4105@o4507016233156608.ingest.us.sentry.io/4507016242003968'
-  config.breadcrumbs_logger = [:active_support_logger, :http_logger]
+  config.breadcrumbs_logger = %i[active_support_logger http_logger]
 
   # Set traces_sample_rate to 1.0 to capture 100%
   # of transactions for performance monitoring.
   # We recommend adjusting this value in production.
   config.traces_sample_rate = 1.0
   # or
-  config.traces_sampler = lambda do |context|
+  config.traces_sampler = lambda do |_context|
     true
   end
 end
