deead5c905bbd9dbaf09d8c490121a639217a612
Oleg Kholyk <oleg@kholyk.ru>
2025-03-10 15:13:15 +0300

    FEAT: auth implemented

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,7 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+  def display_user_email
+    user_signed_in? ? current_user.email : link_to('Sign in', :new_user_session, class: 'text-warning')
+  end
 end
--- app/javascript/application.js
+++ app/javascript/application.js
@@ -7,3 +7,8 @@ import * as bootstrap from "bootstrap"
 // https://getbootstrap.com/docs/5.3/components/popovers/#enable-popovers
 const nodesList = document.querySelectorAll('[data-bs-toggle="popover"]');
 Array.from(nodesList).map(el => new bootstrap.Popover(el, {'trigger': 'focus'}))
+
+// Offcanvas menu
+// https://getbootstrap.com/docs/5.3/components/offcanvas/#via-javascript
+const offCanvasnodelist = document.querySelectorAll('.offcanvas');
+[...offCanvasnodelist].map(el => new bootstrap.Offcanvas(el));
